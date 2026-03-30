679c7456c2fd3eef7bb3d18e7846080a04c66ea3
muskann07 <muskansr.2003@gmail.com>
2026-03-31 00:15:55 +0530

    bitwiseOperator

---
+++ bitwiseOperator.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+
+    //and,or,not,xor
+  //  int a=4;
+  //   int b=6;
+  //   cout<<"a&b"<<(a&b)<<endl;
+  //    cout<<"a|b"<<(a|b)<<endl;
+  //     cout<<"~a"<<~a<<endl;
+  //      cout<<"a^b"<<(a^b)<<endl;
+      
+
+
+      //left shift and right shift
+     cout<<(17>>1)<<endl;
+       cout<<(17>>2)<<endl;
+       cout<<(19<<1)<<endl;
+       cout<<(21<<2)<<endl;
+
+
+       //inc and dec
+
+      //  int i=7;
+
+      //  cout<<(++i)<<endl;
+      //  cout<<(i++)<<endl;
+      //  cout<<(i--)<<endl;
+      //  cout<<(--i)<<endl;
+
+
+       return 0;
+
+
+}
