c193e2b436d5247be8077bb9bc7ddb39576fa7b4
muskann07 <muskansr.2003@gmail.com>
2026-04-06 00:23:59 +0530

    fibonacci series and Prime no.

---
+++ forLoop.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+
+    //sum of n numbers
+
+   /*  int n;
+    cin>>n;
+    int sum=0;
+    for(int i=1;i<=n;i++) {
+        sum+=i;
+    }
+    cout<<sum<<endl;*/ 
+
+
+    //fibonacci series
+
+   /*    int n=10;
+    int a=0;
+    int b=1;
+    cout<<a<<" "<<b<<" ";
+    for(int i=1; i<=n; i++) {
+        int nextNumber=a+b;
+        cout<<nextNumber<<" ";
+        a=b;
+        b=nextNumber;
+    }*/ 
+
+
+    //prime number
+
+    // int n;
+    // cin>>n;
+
+    // bool isPrime=1;
+    // for(int i=2; i<n; i++) {
+    //     if(n%i==0) {
+    //         isPrime=0;
+    //         break;
+    //     }
+    // }
+    // if(isPrime==0) {
+    //     cout<<"Not a prime no."<<endl;
+    // }
+    // else {
+    //     cout<<"is a prime no."<<endl;
+    // }
+    // return 0;
+
+
+    //CONTINUE
+
+   int n;
+    cin>>n;
+    for(int i=0; i<5; i++){
+        cout<<"HI"<<endl;
+        cout<<"HELLO"<<endl;
+        continue;
+        cout<<"reply please"<<endl;
+    }
+    return 0;
+
+}
